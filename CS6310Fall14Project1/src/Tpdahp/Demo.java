efb4ed82fddea2346ed9c6f02bbfaa741f01bc3b
reecekarge <rkarge3@gatech.edu>
2014-09-10 17:43:04 -0400

    Updated with working code

--- CS6310Fall14Project1/src/Tpdahp/Demo.java
+++ CS6310Fall14Project1/src/Tpdahp/Demo.java
@@ -6,8 +6,25 @@ public class Demo {
 		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
-		if(args.length>0)
-			System.out.println(args[0]);
-
+		if(args.length==5){
+			DiffusionSimulator simulator = new DiffusionSimulator(Integer.valueOf(args[0]),
+					Integer.valueOf(args[3]),
+					Integer.valueOf(args[4]),
+					Integer.valueOf(args[1]),
+					Integer.valueOf(args[2]));
+			simulator.simulate();
+			simulator.printPlate();
+		}else{
+			System.out.println("Invalid number of arguements.");
+			System.out.println("Proper usage: java Tpdahp.Demo -d # -l # -r # -t # -b #");
+		}
+//		DiffusionSimulator simulator = new DiffusionSimulator(5,
+//				5,
+//				3,
+//				2,
+//				1);
+//		simulator.simulate();
+//		simulator.printPlate();
+	
 	}
 }
