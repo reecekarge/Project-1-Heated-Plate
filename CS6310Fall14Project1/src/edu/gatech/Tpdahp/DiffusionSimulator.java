97c3a7f537000a4380563dfb5f2fc043422e2eb1
reecekarge <rkarge3@gatech.edu>
2014-09-10 14:07:32 -0400

    Initial checkin

---
+++ CS6310Fall14Project1/src/edu/gatech/Tpdahp/DiffusionSimulator.java
@@ -0,0 +1,14 @@
+package edu.gatech.Tpdahp;
+
+public class DiffusionSimulator {
+
+	public DiffusionSimulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
