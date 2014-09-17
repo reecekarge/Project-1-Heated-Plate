eb9b2261dbd91754904729b6f58c07d3d89c47ec
kimaniidaniel <kdaniel@gatech.edu>
2014-09-17 19:30:07 -0400

    Added GUI2 as a second option.

--- CS6310Fall14Project1/src/Gallhp/Demo.java
+++ CS6310Fall14Project1/src/Gallhp/Demo.java
@@ -2,20 +2,26 @@ package Gallhp;
 
 import javax.swing.SwingUtilities;
 
-public class Demo {
+public class Demo
+{
 
-	public Demo() {
-		// TODO Auto-generated constructor stub
-	}
+    public Demo()
+    {
+        // TODO Auto-generated constructor stub
+    }
 
-	public static void main(String[] args) {
-		  SwingUtilities.invokeLater(new Runnable() {
-		      public void run() {
-		  		DiffusionSimulatorGUI gui = new DiffusionSimulatorGUI();
-				gui.displayGui();
-		      }
-		    });
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                DiffusionSimulatorGUI2 gui = new DiffusionSimulatorGUI2();
+                gui.setVisible(true);
+                //gui.displayGui();
+            }
+        });
 
-	}
+    }
 
 }
