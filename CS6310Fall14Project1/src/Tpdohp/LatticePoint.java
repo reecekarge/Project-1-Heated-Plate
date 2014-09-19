0e54f949f5787623e95166e70e50d416cfd18b26
kimaniidaniel <kdaniel@gatech.edu>
2014-09-19 07:30:05 -0400

    Added LatticePoint structure for Tpdohp

---
+++ CS6310Fall14Project1/src/Tpdohp/LatticePoint.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tpdohp;
+
+/**
+ *
+ * @author kimaniidaniel
+ */
+public class LatticePoint
+{
+
+    LatticePoint _left;
+    LatticePoint _right;
+    LatticePoint _bottom;
+    LatticePoint _top;
+    double _temperature;
+
+    public LatticePoint(LatticePoint _left, LatticePoint _right, LatticePoint _bottom, LatticePoint _top, double _temperature)
+    {
+        this._left = _left;
+        this._right = _right;
+        this._bottom = _bottom;
+        this._top = _top;
+        this._temperature = _temperature;
+    }
+
+    public LatticePoint getBottom()
+    {
+        return _bottom;
+    }
+
+    public LatticePoint getLeft()
+    {
+        return _left;
+    }
+
+    public LatticePoint getRight()
+    {
+        return _right;
+    }
+
+    public double getTemperature()
+    {
+        return _temperature;
+    }
+
+    public LatticePoint getTop()
+    {
+        return _top;
+    }
+}
